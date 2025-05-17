0f3ad2a3fe4a5ca3eb9aec3b69766805f4185450
Noman-QistBazar <mnomanqb@gmail.com>
2025-05-17 18:40:05 +0500

    Update supabase_client.py

--- supabase_client.py
+++ supabase_client.py
@@ -1,7 +1,7 @@
+import streamlit as st
 from supabase import create_client
-import os
 
-SUPABASE_URL = os.getenv("SUPABASE_URL", "https://kwmaeacwhglzzngzwzwr.supabase.co")
-SUPABASE_KEY = os.getenv("SUPABASE_KEY", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt3bWFlYWN3aGdsenpuZ3p3endyIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0NzM5MjY5NCwiZXhwIjoyMDYyOTY4Njk0fQ.Zi4T4TE3caWQPWFhhSiAyI5L6HbabAkaA42-NdS53cI")
+SUPABASE_URL = st.secrets["SUPABASE_URL"]
+SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
 
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
